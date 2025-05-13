c424dc4fa26c1c3b79c33acfd42143424ddba906
Srikanth-Aryasomayajula <venkatasrikantharyasomayajula@gmail.com>
2025-05-13 13:45:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const SHEET_NAME = "Vokabular";
   let allData = [];
 
+	console.log(allData[0]);
   // Fetch and parse Excel
 fetch("Vocabulary.xlsx")
   .then(response => {
