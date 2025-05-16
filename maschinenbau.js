95220df7390482dd757198290798ce9628255c2c
Srikanth-Aryasomayajula <venkatasrikantharyasomayajula@gmail.com>
2025-05-16 12:36:38 +0200

    Update maschinenbau.js

--- maschinenbau.js
+++ maschinenbau.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	  const iframe = document.getElementById("maschinenbauFrame");
 		
   	  if (tableViewRadio.checked) {
-			iframe.src = "maschinenbau_table.html";		  // Enable this if you want maschinenbau_table.html
+			// iframe.src = "maschinenbau_table.html";		  // Enable this if you want maschinenbau_table.html
 			iframe.style.display = "block";   			  // Enable this if you want maschinenbau_table.html (value = 'block' will show the html; value = 'none' will not show the html)
 		    table.style.display = "none"; 			  // Enable this if you don't want the data in maschinenbau_table.html as a plain text (value = 'table' will show the table; value = 'none' will not show the html)
     	    flashcardContainer.style.display = "none";
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
     		table.style.display = "none";
 		    iframe.style.display = "none"; 		
     		flashcardContainer.style.display = "block";
-  		  renderFlashcards(allData);
+  		  	renderFlashcards(allData);
   	  }
   	});
   
