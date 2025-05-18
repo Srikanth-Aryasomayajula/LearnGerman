c3819e1fa8cd319aa1957e2e716f33309b005cdf
Srikanth-Aryasomayajula <venkatasrikantharyasomayajula@gmail.com>
2025-05-18 18:38:50 +0200

    Update ueben.js

--- ueben.js
+++ ueben.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	// Select the topic of practice
 	function loadFlashcards() {
 		loadButton.addEventListener("click", async () => {
-			const selectedSources = document.querySelector('input[name="topic"]:checked');
+			const selectedSources = document.querySelector('input[name="source"]:checked');
 			if (selectedSources.length === 0) {
 				return alert("Please select at least one topic.");
 			}
